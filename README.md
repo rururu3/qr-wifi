b0c2f7b10417761232fb2b978f8e2e61ce3a6029
原澤 直也 <rururulily@gmail.com>
2024-05-13 21:39:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# qr-wifi テキスト仕様
+https://github.com/zxing/zxing/wiki/Barcode-Contents#wi-fi-network-config-android-ios-11
+
+# qrcode.vue
+https://www.npmjs.com/package/qrcode.vue
+
+こっちのほうが機能が多い？
+https://github.com/scholtz/qrcode-vue3
---
+++ src/components/QrCodeRender.vue
@@ -0,0 +1,19 @@
+<template>
+  <qrcode-vue :value="wifiValue" :level="level" :render-as="renderAs" :size="size" />
+</template>
+
+<script setup lang="ts">
+  import { ref, computed } from 'vue'
+  import QrcodeVue, { type Level, type RenderAs } from 'qrcode.vue'
+
+  const mynetwork = ref('mynetwork')
+  const password = ref('password')
+  const level = ref<Level>('H')
+  const renderAs = ref<RenderAs>('canvas')
+  const size = ref(200)
+
+  // WiFi用QRCodeテキスト生成
+  const wifiValue = computed(() => {
+    return('WIFI:S:"' + mynetwork.value + '";T:WPA;P:"' + password.value + '";;')
+  });
+</script>
